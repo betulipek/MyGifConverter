f622e2e11fdab597c74c152f52da7fdc53dde355
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-22 16:21:25 +0300

    start 5 - end 10 changed in the project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from moviepy.editor import  *
 
 clip_mp4 = VideoFileClip("Madonna.mp4")
-clip_mp4 = clip_mp4.subclip(0, 3)
+clip_mp4 = clip_mp4.subclip(5, 10)
 
 clip_gif = clip_mp4.write_gif("Madonna.gif")
 
